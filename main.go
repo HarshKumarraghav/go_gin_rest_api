09f19c8b3139a875e3d74846c2ba60d646f49f09
Harsh Kumar Raghav <raghavharsh068@gmail.com>
2023-01-27 03:06:57 +0530

    basic setup done

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	godotenv.Load()
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run("localhost:" + os.Getenv("PORT"))
+
 }
